581ca394719c63a5f453cdf6cc9dd6b180dfcfea
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-04-24 16:15:22 -0300

    feat: creation tests for checkout

--- src/lib/Cart.spec.js
+++ src/lib/Cart.spec.js
@@ -3,40 +3,104 @@ import Cart from './Cart';
 describe('Cart', () => {
   let cart;
   let product = {
-    title: 'Adidas Running Shoes',
+    title: 'Adidas Running Shoes - man',
     price: 35388,
+  };
+
+  let product2 = {
+    title: 'Adidas Running Shoes - woman',
+    price: 41872,
   };
 
   beforeEach(() => {
     cart = new Cart();
   });
 
-  it('should return 0 when getTotal() is a newly created instance', () => {
-    expect(cart.getTotal()).toEqual(0);
-  });
-
-  it('should multiply quantity and price and receive the total amount', () => {
-    const item = {
-      product,
-      quantity: 2,
-    };
-
-    cart.add(item);
-
-    expect(cart.getTotal()).toEqual(70776);
-  });
-
-  it('should ensure no more than on product exists as a time', () => {
-    cart.add({
-      product,
-      quantity: 2,
+  describe('getTotal()', () => {
+    it('should return 0 when getTotal() is a newly created instance', () => {
+      expect(cart.getTotal()).toEqual(0);
     });
 
-    cart.add({
-      product,
-      quantity: 1,
+    it('should multiply quantity and price and receive the total amount', () => {
+      const item = {
+        product,
+        quantity: 2,
+      };
+
+      cart.add(item);
+
+      expect(cart.getTotal()).toEqual(70776);
     });
 
-    expect(cart.getTotal()).toEqual(35388);
+    it('should ensure no more than on product exists as a time', () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      cart.add({
+        product,
+        quantity: 1,
+      });
+
+      expect(cart.getTotal()).toEqual(35388);
+    });
+
+    it('should update total when a product gets included and then removed', () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 1,
+      });
+
+      cart.remove(product);
+
+      expect(cart.getTotal()).toEqual(41872);
+    });
+  });
+
+  describe('checkout()', () => {
+    it('should return an object with the total and the list of item', () => {
+      cart.add({
+        product,
+        quantity: 5,
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 3,
+      });
+
+      expect(cart.checkout()).toMatchSnapshot();
+    });
+
+    it('should return an object with the total and the list of items when summary() is called', () => {
+      cart.add({
+        product,
+        quantity: 5,
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 3,
+      });
+
+      expect(cart.summary()).toMatchSnapshot();
+      expect(cart.getTotal()).toBeGreaterThan(0);
+    });
+
+    it('should reset the cart when checkout() is called', () => {
+      cart.add({
+        product: product2,
+        quantity: 3,
+      });
+      cart.checkout();
+
+      expect(cart.getTotal()).toEqual(0);
+    });
   });
 });
